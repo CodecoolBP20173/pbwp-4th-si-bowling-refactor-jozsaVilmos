ac0f4c786ddd774064c4d5c9392db9f1d052536d
Jozsa Vilmos <vilixjo@gmail.com>
2017-11-29 16:19:02 +0100

    get value cleaned , spare checking added

--- bowling.py
+++ bowling.py
@@ -1,4 +1,5 @@
 def score(game):
+    game = game.lower()
     result = 0
     frame = 1
     in_first_half = True
@@ -7,39 +8,38 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
+
+        last = get_value(game[i])
+
+        if frame < 10  and last == 10:
+            if is_spare(game[i]):  # if is_spare(game[i])
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i] == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-        last = get_value(game[i])
+
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+
+        in_first_half = not in_first_half
+
+        if game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    if char in ['x' , '/']:
         return 10
     elif char == '-':
         return 0
-    else:
-        raise ValueError()
+    elif int(char) in range(1, 10):
+        return int(char)
+
+    raise ValueError()
+
+def is_spare(score):
+    return score == '/'
